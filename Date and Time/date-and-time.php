5abb0c1f5fc1f3adbc617b27ffe8d63786da707b
Gourav Auluck <gouravauluck@MNG-IT-LP2981.local>
2022-04-11 21:09:51 -0700

    Date and Time

--- Date and Time/date-and-time.php
+++ Date and Time/date-and-time.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "hello world";
+// echo "hello world";
+
+// Date
+// d - day of the month
+// m - month
+// Y - year
+// l - day of the week
+
+echo date('m - d - Y - l');
+
+echo "<br>";
+
+//  Time
+
+// H - Hour w/24 hour format
+// h - Hour w/12 hour format
+// i - Minutes with leading zeroes
+// s - Seconds with leading zeroes
+// a - am or pm
+
+date_default_timezone_set('America/Los_Angeles');
+
+echo date_default_timezone_get();
+
+echo "<br>";
+
+echo date('H - h - i - s - a');
